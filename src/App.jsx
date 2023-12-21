126eb3480121b08eb18b637724489b595bae2d58
19Rupa <rupalik1019@gmail.com>
2023-12-21 17:27:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-800">
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
